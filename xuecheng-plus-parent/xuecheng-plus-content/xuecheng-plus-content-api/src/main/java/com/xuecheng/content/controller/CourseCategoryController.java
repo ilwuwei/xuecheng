a257701f0358c8ba3419d4c22cfeade33aeb41a9
ilwuwei <ilwuwei@126.com>
2023-03-05 22:05:37 +0800

    day 课程分类查询 添加课程

---
+++ xuecheng-plus-parent/xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/controller/CourseCategoryController.java
@@ -0,0 +1,27 @@
+package com.xuecheng.content.controller;
+
+import com.xuecheng.content.model.dto.CourseCategoryTreeDto;
+import com.xuecheng.content.service.CourseCategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@Api(tags = "课程分类管理接口")
+public class CourseCategoryController {
+
+    @Autowired
+    CourseCategoryService courseCategoryService;
+
+    @ApiOperation(value = "课程分类查询")
+    @GetMapping("/course-category/tree-nodes")
+    public List<CourseCategoryTreeDto> queryTreeNodes() {
+        return courseCategoryService.queryTreeNodes("1");
+    }
+}
